39a2c545c3afa1742c441f3cd2fb84ca83637f5a
Gordei Evgenii <gordei.evgenii@iqubecompany.com>
2019-06-21 00:48:12 +0300

    modal plagin pattern

--- Task18(Modal Plagin MVC)/app.js
+++ Task18(Modal Plagin MVC)/app.js
@@ -1,178 +1,127 @@
-// MODEL
-let model = {
-	dataObj: {
-		name: null,
-		day: null,
-		month: null,
-		year: null
-	},
-	modalState: function (state) {
-		view.modalState(state);
-	},
-	setData: function (key) {
-		localStorage.setItem(key, JSON.stringify(this.dataObj));
-	},
-	getData: function (key) {
-		return JSON.parse(localStorage.getItem(key));
-	},
-	removeData: function (key) {
-		localStorage.removeItem(key);
-	},
-	showInfo: function (key) {
-		let data = model.getData(key)
-		view.showInfo(data);
-	},
-	removeInfo: function (key) {
-		model.removeData(key)
-		view.removeInfo();
-	}
-};
-
-// VIEW
-let view = {
-	modal: document.querySelector('.modal'),
-	targetBox: document.querySelector(".target"),
-	modalState: function (state) {
-		let overlay = document.querySelector('.modal-overlay');
-
-		if (state === 'open') {
-			overlay.classList.remove('modal_closed');
-			view.modal.classList.remove('modal_closed');
-		}
-		if (state === 'close') {
-			overlay.classList.add('modal_closed');
-			view.modal.classList.add('modal_closed');
-		}
-	},
-	showInfo: function (data) {
-		let helloStr = data.name ? `Привет, ${data.name}!` : `Ваше имя не известно =(`;
-		let birthdayStr = (data.day && data.month && data.year) ? `У тебя ДР ${data.day}/${data.month}/${data.year}` : `Дата ДР неизвестна`;
-
-		view.targetBox.innerHTML = `<p>${helloStr}</p>` +
-			`<p>${birthdayStr}</p>` +
-			`<a href='#' class='clearData'>Очистить данные</a>`;
-	},
-	removeInfo: function () {
-		view.targetBox.innerHTML = `<p>Данные отсутствуют</p>`;
-	}
-};
-
-// CONTROLLER
-let controller = {
-	events: function () {
-		let btnOpen = document.querySelector('.modal-open');
-		let btnClose = document.querySelector('.modal__close')
-		let btnCancel = document.querySelector('.modal__cancel');
-		let overlay = document.querySelector('.modal-overlay');
-		let btnSave = document.querySelector('#modal-save');
-
-		btnOpen.addEventListener('click', function (e) {
-			e.preventDefault();
-			controller.handleButtonOpen();
-		});
-		btnClose.addEventListener('click', function (e) {
-			e.preventDefault();
-			controller.handleButtonClose();
-		});
-		btnCancel.addEventListener('click', function (e) {
-			e.preventDefault();
-			controller.handleButtonClose();
-		});
-		overlay.addEventListener('click', function (e) {
-			controller.handleButtonClose();
-		});
-		btnSave.addEventListener('click', function (e) {
-			controller.handleSaveData();
-		});
-		if (localStorage.getItem('userData')) {
-			model.showInfo('userData');
-			controller.handleRemoveInfo();
-		};
-	},
-	clearModalInputs: function () {
-		let inputs = document.querySelectorAll('.modal input');
-
-		for (let i = 0; i < inputs.length; i++) {
-			inputs[i].value = '';
-		}
-	},
-	handleButtonOpen: function () {
-		model.modalState('open');
-	},
-	handleButtonClose: function () {
-		controller.clearModalInputs();
-		model.modalState('close');
-	},
-	handleSaveData: function () {
-		let inputName = document.querySelector('#name');
-		let inputDay = document.querySelector('#birth-day');
-		let inputMonth = document.querySelector('#birth-month');
-		let inputYear = document.querySelector('#birth-year');
-
-		model.dataObj.name = inputName.value;
-		model.dataObj.day = inputDay.value;
-		model.dataObj.month = inputMonth.value;
-		model.dataObj.year = inputYear.value;
-
-		if (model.dataObj.name && model.dataObj.day && model.dataObj.month && model.dataObj.year) {
-			model.setData('userData');
-			model.modalState('close');
-			model.showInfo('userData');
-		} else {
-			alert('Необходимо заполнить все поля!');
-		}
-	},
-	handleRemoveInfo: function () {
-		let clearBtn = document.querySelector('.clearData');
-
-		clearBtn.addEventListener("click", function (e) {
-			e.preventDefault();
-			model.removeInfo('userData');
-		});
-	},
-};
-
-(function () {
-
-	let app = {
-
-		init: function () {
-			this.main();
-		},
-
-		main: function () {
-			controller.events();
-		}
-	}
-
-	app.init();
-
-}());
-
-
-
-
-
-
 const CoolModal = (function () {
 
-	let opt = {
+	this.settings = {
 		name: 'default name',
-	}
-
-	function _setOpts(userOpts) {
-		opt.name = userOpts.name;
-	}
-
-	function _privateMethod() {
-		console.log(opt.name);
 	};
-	
+
+	this._setSettings = function (userSettings) {
+		settings.name = userSettings.name;
+	};
+
+	// MODEL
+	let model = {
+		// открытие / закрытие модалки
+		modalState: function (state, linkId) {
+			view.modalState(state, linkId);
+		},
+		// закрывает все модалки
+		modalCloseAll: function () {
+			view.modalCloseAll();
+		} ,
+		// определяет тип линки и решает какую модалку открывать
+		checkModalType: function (link) {
+			if (link.hasAttribute('data-supermodal') && link.hasAttribute('data-supermodal-title') && link.hasAttribute('data-supermodal-content')) {
+				console.log(3);
+			}
+			else if (link.hasAttribute('data-supermodal-title') && link.hasAttribute('data-supermodal-content')) {
+				let title = link.getAttribute('data-supermodal-title');
+				let content = link.getAttribute('data-supermodal-content');
+				view.modalPushAdd(title, content);
+				model.modalPushClose();
+			}
+			else if (link.hasAttribute('data-supermodal')) {
+				controller.handleLinkOpen(link.dataset.supermodal);
+			}
+			else {
+				console.log('а нифига хорошего');
+			}
+		},
+		// закрывает пуш модалку
+		modalPushClose: function() {
+			controller.handlePushClose();
+		}
+	};
+
+	// VIEW
+	let view = {
+		overlay: document.querySelector('.modal-overlay'),
+		modalState: function (state, linkId) {
+			let modal = document.querySelector(`#${linkId}`);
+			if (state === 'open') {
+				view.overlay.classList.remove('modal_closed');
+				modal.classList.remove('modal_closed');
+			}
+			if (state === 'close') {
+				view.overlay.classList.add('modal_closed');
+				modal.classList.add('modal_closed');
+			}
+		},
+		modalCloseAll: function () {
+			let allModals = document.querySelectorAll('.modal');
+			allModals.forEach((item) => {
+				view.overlay.classList.add('modal_closed');
+				item.classList.add('modal_closed');
+			});
+		},
+		// добавляет в боди пуш модалку
+		modalPushAdd: function (title, content) {
+			let pushHtml = `<div class='modal-push'>` + 
+								`<div class='modal-push__title'>${title}</div>` +
+								`<div class='modal-push__content'>${content}</div>` +
+							`</div>`
+			document.body.insertAdjacentHTML("beforeEnd", pushHtml);
+		}
+	};
+
+	// CONTROLLER
+	let controller = {
+		events: function () {
+			let openLink = document.querySelectorAll('.container a');
+			let btnClose = document.querySelectorAll('.modal__close');
+			let overlay = document.querySelector('.modal-overlay');
+
+			openLink.forEach((link) => {
+				link.addEventListener('click', function (e) {
+					e.preventDefault();
+					// передаём линку в модель для определения типа
+					model.checkModalType(link);
+				});
+			})
+
+			// btnClose[i].addEventListener('click', function (e) {
+			// 	e.preventDefault();
+			// 	controller.handleButtonClose(openLink[i].dataset.supermodal);
+			// });
+
+			overlay.addEventListener('click', function (e) {
+				controller.handleOverlayClose();
+			});
+		},
+
+		handleLinkOpen: function (linkId) {
+			model.modalState('open', linkId);
+		},
+		handleButtonClose: function (linkId) {
+			model.modalState('close', linkId);
+		},
+		handleOverlayClose: function (linkId) {
+			model.modalCloseAll();
+		},
+		// убирает пушмодалаку
+		handlePushClose: function() {
+			let modalPush = document.querySelectorAll('.modal-push');
+			modalPush.forEach((item) => {
+				setTimeout(function() {
+					item.remove();
+				}, 3000)
+			}) 
+		}
+	};
 
 	let app = {
-		publicMethod: function (userOpts) {
-			_setOpts(userOpts);
-			_privateMethod();
+		init: function (userSettings) {
+			_setSettings(userSettings);
+			controller.events();
 		},
 	}
 
@@ -180,6 +129,6 @@ const CoolModal = (function () {
 
 })();
 
-CoolModal.publicMethod({
+CoolModal.init({
 	name: 'name1234',
 });
